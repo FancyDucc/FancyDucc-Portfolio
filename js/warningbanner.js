a3f22fc4ce4ec77462820fbc29b4f2305b1f1f32
Fancy Ducc <aweeaaweee@gmail.com>
2025-02-23 19:40:24 -0800

    warning banner and temporary navigation
    don't you love it when debugging in VS code works perfect, but then the pushed website doesn't?
    
    yeah, me too.

---
+++ js/warningbanner.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var bannerEnabled = true;
+    var defaultBannerContent = '<div class="container text-center" style="color: rgb(255, 255, 255);">' +
+      '<span id="warningBannerText">This entire website is under a full redesign and because it is all frontend coded I need do everything manually; So expect things to not work, go here for temporary navigation if the navigation bar is not working: ' +
+      '<a href="temporarynavigation.html" class="hover-link-white" style="text-decoration:underline;">Temporary Navigation</a>' +
+      '</span>' +
+      '</div>';
+    var bannerBgColor = "rgb(80, 0, 230)";
+
+
+
+
+
+
+    if (!bannerEnabled) return;
+  
+    var banner = document.createElement("div");
+    banner.id = "warningBanner";
+    banner.style.position = "fixed";
+    banner.style.top = "0";
+    banner.style.left = "0";
+    banner.style.width = "100%";
+    banner.style.backgroundColor = bannerBgColor;
+    banner.style.color = "#ffffff";
+    banner.style.padding = "10px 0";
+    banner.style.zIndex = "1100";
+  
+    banner.innerHTML = defaultBannerContent;
+  
+    document.body.insertBefore(banner, document.body.firstChild);
+  
+    var mainNav = document.getElementById("mainNav");
+    if (mainNav) {
+      mainNav.style.top = banner.offsetHeight + "px";
+    }
+  });
+
+  function updateWarningBannerContent(newContent) {
+    var bannerTextElem = document.getElementById("warningBannerText");
+    if (bannerTextElem) {
+      bannerTextElem.innerHTML = newContent;
+    }
+  }
+  
+  function toggleWarningBanner(show) {
+    var banner = document.getElementById("warningBanner");
+    if (banner) {
+      banner.style.display = show ? "block" : "none";
+      var mainNav = document.getElementById("mainNav");
+      if (mainNav) {
+        mainNav.style.top = show ? banner.offsetHeight + "px" : "0px";
+      }
+    }
+  }
